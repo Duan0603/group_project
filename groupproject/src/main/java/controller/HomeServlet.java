4582cecb27002e69b5f423141d8b80b67c17bb81
tungx11925 <tnt11925@gmail.com>
2025-06-25 03:58:15 +0700

    Update HomeServlet.java

--- groupproject/src/main/java/controller/HomeServlet.java
+++ groupproject/src/main/java/controller/HomeServlet.java
@@ -1,30 +1,3 @@
-//package controller;
-//
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.*;
-//import java.io.IOException;
-//import model.User;
-//
-//@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
-//public class HomeServlet extends HttpServlet {
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//
-//        HttpSession session = request.getSession();
-//        User user = (User) session.getAttribute("user");
-//
-//        if (user == null) {
-//            response.sendRedirect(request.getContextPath() + "/login");
-//            return;
-//        }
-//
-//        request.setAttribute("user", user);
-//        request.getRequestDispatcher("WEB-INF/views/home.jsp").forward(request, response);
-//    }
-//}
 package controller;
 
 import jakarta.servlet.ServletException;
